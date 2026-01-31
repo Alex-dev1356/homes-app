f55221677266850ad337967b12307c87f649638a
Alex-dev1356 <asoliven26@yahoo.com>
2026-01-31 19:46:50 +0800

    Creating a new type to represent our data structure using an Interface

---
+++ src/app/housing-location.ts
@@ -0,0 +1,11 @@
+export interface HousingLocation {
+    //These are the following properties that describes our data
+    id: number,
+    name: string,
+    city: string,
+    state: string,
+    photo: string,
+    availableUnits: number,
+    wifi: boolean,
+    laundry: boolean
+}
