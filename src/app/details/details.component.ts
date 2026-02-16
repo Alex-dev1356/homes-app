57e9749dc7382038841b7fff0a74397e8a289925
Alex-dev1356 <asoliven26@yahoo.com>
2026-02-16 23:48:26 +0800

    Utilizing the service that we created

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
@@ -8,7 +10,8 @@ import { ActivatedRoute } from '@angular/router';
   imports: [CommonModule],
   template: `
     <p>
-      details works! {{housingLicationId}}
+      <!-- Making the ID optional -->
+      details works! {{housingLocation?.id}}
     </p>
   `,
   styleUrls: ['./details.component.css']
@@ -17,12 +20,15 @@ export class DetailsComponent {
   //The ActivatedRoute is a reference to the current route we're matched against in the 
   //application
   route: ActivatedRoute = inject(ActivatedRoute);
-  //Creating another property
-  housingLicationId = 0;
+
+  housingService: HousingService = inject(HousingService);
+
+  housingLocation: HousingLocation | undefined;
 
   //Creating a method
   constructor()
   {
-    this.housingLicationId = Number(this.route.snapshot.paramMap.get('id'));
+    const housingLicationId = Number(this.route.snapshot.paramMap.get('id'));
+    this.housingLocation  = this.housingService.getHousingLocationById(housingLicationId);
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -112,4 +112,9 @@ export class HousingService {
   getAllHousingLocations() : HousingLocation[] {
     return this.housingLocationList;
   }
+
+  getHousingLocationById(id: number) : HousingLocation | undefined
+  {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
 }
